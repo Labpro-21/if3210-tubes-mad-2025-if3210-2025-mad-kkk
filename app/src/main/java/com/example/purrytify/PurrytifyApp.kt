f4e8a56ee4bc07edc4fb17872ed2dfb61eecd0ab
ninoaddict <adrilbless37@gmail.com>
2025-04-08 17:49:03 +0700

    fix: navigation railbar

--- app/src/main/java/com/example/purrytify/PurrytifyApp.kt
+++ app/src/main/java/com/example/purrytify/PurrytifyApp.kt
@@ -6,15 +6,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -73,6 +78,9 @@ fun PurrytifyApp(
         Row(Modifier.fillMaxSize()) {
             AnimatedVisibility(visible = (navigationType == PurrytifyNavigationType.NAVIGATION_RAIL && hasNavbar)) {
                 NavigationRailBar(navController)
+            }
+            AnimatedVisibility(visible = (navigationType == PurrytifyNavigationType.NAVIGATION_RAIL && hasNavbar)) {
+                VerticalDivider(modifier = Modifier.padding(WindowInsets.statusBars.asPaddingValues()))
             }
             Column(
                 modifier = Modifier
--- app/src/main/java/com/example/purrytify/ui/component/NavigationRailBar.kt
+++ app/src/main/java/com/example/purrytify/ui/component/NavigationRailBar.kt
@@ -1,11 +1,25 @@
 package com.example.purrytify.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.NavigationRailItemColors
+import androidx.compose.material3.NavigationRailItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.purrytify.navigation.Screen
@@ -17,11 +31,21 @@ fun NavigationRailBar(navController: NavHostController, modifier: Modifier = Mod
 
     NavigationRail(modifier = modifier) {
         items.forEach { screen ->
+            val selected = currentRoute == screen.route
             NavigationRailItem(
-                selected = currentRoute == screen.route,
+                selected = selected,
                 onClick = { navController.navigate(screen.route) },
-                icon = { Icon(screen.icon, contentDescription = screen.title) },
-                label = { Text(screen.title) }
+                icon = {
+                    Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(start = 8.dp).width(176.dp)) {
+                        Icon(screen.icon, contentDescription = screen.title, modifier = Modifier.size(30.dp))
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(text = screen.title, fontSize = 16.sp)
+                    }
+                },
+                alwaysShowLabel = false,
+                colors = NavigationRailItemDefaults.colors(
+                    indicatorColor = Color.Transparent
+                )
             )
         }
     }
