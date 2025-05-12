75f7377782b4383c17567e03a603cd4b4a2d7854
ninoaddict <adrilbless37@gmail.com>
2025-05-12 22:59:31 +0700

    feat: add online song service to fetch songs

---
+++ app/src/main/java/com/example/purrytify/service/OnlineSongService.kt
@@ -0,0 +1,24 @@
+package com.example.purrytify.service
+
+import retrofit2.http.GET
+
+interface OnlineSongService {
+    @GET("api/top-songs/global")
+    suspend fun topGlobalSongs(): List<OnlineSongResponse>
+
+    @GET("api/top-songs/:country")
+    suspend fun topCountrySongs(country: String): List<OnlineSongResponse>
+}
+
+data class OnlineSongResponse (
+    val id: Int,
+    val title: String,
+    val artist: String,
+    val artwork: String,
+    val url: String,
+    val duration: String,
+    val country: String,
+    val rank: Int,
+    val createdAt: String,
+    val updatedAt: String
+)
